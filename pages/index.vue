4995d841af61482f17b51584c81147262843a695
getmoredigital <conrad@getmore.digital>
2022-11-24 07:27:04 -0700

    Return to normal for testing

--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,10 @@ definePageMeta({  middleware: 'auth'})
 
 const logout = () => {
     auth.signOut()
-    const accessToken = useCookie('sb-access-token')
-    const refreshToken = useCookie('sb-refresh-token')
-    accessToken.value = null
-    refreshToken.value = null
+    // const accessToken = useCookie('sb-access-token')
+    // const refreshToken = useCookie('sb-refresh-token')
+    // accessToken.value = null
+    // refreshToken.value = null
     router.push('/login')
 }
 
