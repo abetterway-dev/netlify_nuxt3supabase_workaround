e36003bed84cf2089fbcdf2d0a8f1ce903cd07ec
getmoredigital <conrad@getmore.digital>
2022-11-24 07:42:44 -0700

    test #5

--- server/api/test-fix.ts
+++ server/api/test-fix.ts
@@ -1,7 +1,12 @@
 import { setCookie } from 'h3'
 // import { serverSupabaseUser } from '#supabase/server'
 export default defineEventHandler(async (event) => {
-    setCookie(event, `test-access-token`, 'VALUE HERE')
+    setCookie(event, `test-access-token`, 'VALUE HERE', {
+        domain: 'https://meek-phoenix-1f6073.netlify.app',
+        maxAge: 60 * 60 * 8,
+        path: '/',
+        sameSite: 'lax'
+      })
 
     return 'done'
 })
