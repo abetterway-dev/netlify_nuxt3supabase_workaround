b749c314cb2a6267366bf60eae5eb56c24a2ba7c
getmoredigital <conrad@getmore.digital>
2022-11-24 07:36:21 -0700

    test #2

---
+++ server/api/test-fix.ts
@@ -0,0 +1,12 @@
+import { setCookie } from 'h3'
+// import { serverSupabaseUser } from '#supabase/server'
+export default defineEventHandler(async (event) => {
+    setCookie(event, `test-access-token`, 'VALUE HERE', {
+        domain: '',
+        maxAge: 60 * 60 * 8,
+        path: '/',
+        sameSite: 'lax'
+      })
+
+    return 'done'
+})
