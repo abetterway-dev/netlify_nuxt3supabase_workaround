15a921ddc7467359edcd1e668168cef2ed5eb972
getmoredigital <conrad@getmore.digital>
2022-11-23 22:08:14 -0700

    first commit

---
+++ pages/login.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+const { auth } = useSupabaseAuthClient()
+const user = useSupabaseUser()
+
+const password = ref("")
+const email = ref("")
+const errorMsg = ref("")
+const login = async () => {
+    const loginRequest = {
+                email: email.value,
+                password: password.value
+            }
+
+  const { data: response, error } = await auth.signInWithPassword(loginRequest)
+    if(!!response){
+        const accessToken = useCookie('sb-access-token')
+        const refreshToken = useCookie('sb-refresh-token')
+        accessToken.value = response.session?.access_token ?? null
+        refreshToken.value = response.session?.refresh_token ?? null
+    }
+
+    if(!!error){
+        errorMsg.value = error.message
+    }
+}
+    
+    watch(user,() => {
+        if(!!user.value){
+            navigateTo('/')
+        }
+    })
+</script>
+<template>
+    <div>
+        <label>E-Mail: </label>
+        <input type="text" v-model="email" />
+    </div>
+    <div>
+        <label>Password: </label>
+        <input type="password" v-model="password" />
+    </div>
+    <button @click="login">Login</button>
+    <div>{{ errorMsg }}</div>
+</template>
