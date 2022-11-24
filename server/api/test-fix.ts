f1cbd1932152164f75c92f75138898a965893b40
getmoredigital <conrad@getmore.digital>
2022-11-24 09:05:32 -0700

    related to _secure

--- server/api/test-fix.ts
+++ server/api/test-fix.ts
@@ -5,9 +5,13 @@ export default defineEventHandler(async (event) => {
     const cookieStr = serialize('test-access-token', 'Work Around', {
         path: '/',
         maxAge: 60 * 60 * 8,
+        secure: true,
+        httpOnly: true,
       })
     console.log('test')
     appendResponseHeaders(event, { "Set-Cookie": cookieStr  })
+    appendResponseHeaders(event, { "Set-Cookie": 'test=hello world'  })
+    appendResponseHeaders(event, { "Set-Cookie": '_Secure-test=hello world'  })
     appendResponseHeaders(event, { "other": cookieStr  })
     
     return 'working'
