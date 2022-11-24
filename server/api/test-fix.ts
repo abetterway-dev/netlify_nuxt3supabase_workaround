7d807dff02d0b3d47baa8435042993514f327279
getmoredigital <conrad@getmore.digital>
2022-11-24 08:56:09 -0700

    removed more options

--- server/api/test-fix.ts
+++ server/api/test-fix.ts
@@ -2,8 +2,6 @@ import { serialize } from "cookie-es";
 import { appendResponseHeaders } from 'h3'
 export default defineEventHandler(async (event) => {
     const cookieStr = serialize('test-access-token', 'Work Around', {
-        secure: true,
-        httpOnly: true,
         path: '/',
         maxAge: 60 * 60 * 8,
       })
